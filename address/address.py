9abe80189555b0b874809d8d45e20efcd1bf31ff
Nicholas Canzoneri <ac3522@gmail.com>
2011-08-29 23:34:56 -0400

    Adding data for more cities and changing how images get created

--- address/address.py
+++ address/address.py
@@ -1,30 +1,41 @@
 import mapnik
 
-m = mapnik.Map(1500, 1500, "+proj=latlong +datum=NAD83")
-
+#base map setup
+m = mapnik.Map(2500, 2500, "+proj=latlong +datum=NAD83")
 m.background = mapnik.Color('#111')
 
-s = mapnik.Style()
-
+#base line styles
 r = mapnik.Rule()
-
 r.symbols.append(mapnik.LineSymbolizer(mapnik.Color('#FFF'),0.1))
 
+#line styles for blocks 0 to 100
 where_filter = "([LFROMADDI] <= 100 and [LFROMADDI] > 0) or ([RFROMADDI] <= 100 and [RFROMADDI] > 0)"
 filter_rule = mapnik.Rule()
 filter_rule.filter = mapnik.Filter(where_filter)
 filter_rule.symbols.append(mapnik.LineSymbolizer(mapnik.Color('#cb50ee'),0.5))
 
+#adding styles
+s = mapnik.Style()
 s.rules.append(r)
 s.rules.append(filter_rule)
-
 m.append_style('My Style', s)
 
-lyr = mapnik.Layer('world', "+proj=latlong +datum=NAD83")
-lyr.datasource = mapnik.Shapefile(file='/Users/ncanzone/development/mapnik_examples/data/philly/tl_2010_42101_edges')
-lyr.styles.append('My Style')
+#add shape information for specific cities
+inputs = [
+  ("philly/tl_2010_42101_edges", "philly.png"),
+  ("portland/tl_2010_41051_edges", "portland.png"),
+  ("newyork/tl_2010_36061_edges", "newyork.png"),
+  ("boston/tl_2010_25025_edges", "boston.png"),
+]
+base_data = "/Users/ncanzone/development/mapnik_examples/data/"
 
-m.layers.append(lyr)
-m.zoom_to_box(lyr.envelope())
+def make_map(shpfile, img_name):
+  lyr = mapnik.Layer('world', "+proj=latlong +datum=NAD83")
+  lyr.datasource = mapnik.Shapefile(file=base_data + shpfile)
+  lyr.styles.append('My Style')
+  m.layers.append(lyr)
+  m.zoom_to_box(lyr.envelope())
+  mapnik.render_to_file(m, img_name, 'png')
 
-mapnik.render_to_file(m, 'address.png', 'png')
+for shpfile, img_name in inputs:
+  make_map(shpfile, img_name)
