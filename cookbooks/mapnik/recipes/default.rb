edfdcf4301521f6e915a5c784f15bd53bf950730
Nicholas Canzoneri <ac3522@gmail.com>
2011-09-08 21:58:17 -0400

    Adding vagrantfile and cookbook for provisioning

---
+++ cookbooks/mapnik/recipes/default.rb
@@ -0,0 +1,13 @@
+%w{ python-setuptools libmapnik0.7 mapnik-utils python-mapnik }.each do |pkg|
+  apt_package pkg do
+    action :install
+  end
+end
+
+bash "install tilelite server" do
+  cwd "/tmp"
+  code <<-EOH
+  sudo easy_install tilelite
+  EOH
+end
+
