b0c9478e735f3416899382a58059c81d4e46d560
Nicholas Canzoneri <ac3522@gmail.com>
2011-09-08 23:55:13 -0400

    Adding rake as a dependency to vagrant

--- cookbooks/mapnik/recipes/default.rb
+++ cookbooks/mapnik/recipes/default.rb
@@ -1,4 +1,4 @@
-%w{ python-setuptools libmapnik0.7 mapnik-utils python-mapnik }.each do |pkg|
+%w{ rake python-setuptools libmapnik0.7 mapnik-utils python-mapnik }.each do |pkg|
   apt_package pkg do
     action :install
   end
