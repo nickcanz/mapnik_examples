79bb419747c971397aaba4fe62fe709d332e67df
Nicholas Canzoneri <ac3522@gmail.com>
2011-09-10 21:43:58 -0400

    Updating mapnik recipe to include gdal

--- cookbooks/mapnik/recipes/default.rb
+++ cookbooks/mapnik/recipes/default.rb
@@ -1,4 +1,4 @@
-%w{ python-werkzeug rake python-setuptools libmapnik0.7 mapnik-utils python-mapnik }.each do |pkg|
+%w{ gdal-bin python-werkzeug rake python-setuptools libmapnik0.7 mapnik-utils python-mapnik }.each do |pkg|
   apt_package pkg do
     action :install
   end
